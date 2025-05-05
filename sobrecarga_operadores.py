2519a4c7a905b804424134dfd558eea2cdee52ab
Matias Daniel Alessandrello <matialess@gmail.com>
2025-05-05 14:28:00 -0300

    Sobrecarga de operadores

---
+++ sobrecarga_operadores.py
@@ -0,0 +1,19 @@
+# operador de suma
+a = 2
+b = 3
+print(a+b)
+
+a = 'Hola '
+b = 'Mundo'
+print(a+b)
+
+# Si tenemos tipos enteros se comporta de una forma y si tenmos tipo string se comporta de otra el operador suma 
+
+a = [1,2,3]
+b = [6,7,8]
+
+print(a + b)
+
+# con las listas tambien funciona de otra forma
+
+# la sobrecarga significa que tiene diferentes comportamientos dependiendo los operandos con los que se este trabajando
